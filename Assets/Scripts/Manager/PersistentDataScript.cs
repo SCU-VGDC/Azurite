e29a80c2976cea8b94cb7e4795a572a250036b6b
kieranpazmino <42kpaz@gmail.com>
2024-10-18 17:19:29 -0700

    Renamed Scripts
    Renamed scripts for broader future use.

--- Assets/Scripts/Manager/PersistentDataScript.cs
+++ Assets/Scripts/Manager/PersistentDataScript.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomStateManager : MonoBehaviour
+public class PersistentDataScript : MonoBehaviour
 {
-    public static RoomStateManager instance;
+    public static PersistentDataScript instance;
 
     public int worldState; //Sets the world state. All rooms should call this to determine their state.
     //0 - Default World State
@@ -19,17 +19,6 @@ public class RoomStateManager : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(this.gameObject);
-        worldState = 0;
-
-    }
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 }
---
+++ Assets/persistenceDebug.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class persistenceDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer m_SpriteRenderer;
+    //The Color to be assigned to the Renderer’s Material
+    Color m_NewColor;
+    //These are the values that the Color Sliders return
+    float m_Red, m_Blue, m_Green;
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        PersistentDataScript.instance.worldState = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PersistentDataScript.instance.worldState == 2)
+        {
+            m_SpriteRenderer.color = Color.green;
+        }
+        else
+        {
+            m_SpriteRenderer.color = Color.red;
+        }
+
+    }
+}
