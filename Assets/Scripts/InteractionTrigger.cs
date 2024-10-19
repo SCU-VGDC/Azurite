1158290f9eda63f9897aaa59da22beda29419c52
Viziaxi <56894726+Viziaxi@users.noreply.github.com>
2024-10-18 18:31:45 -0700

    Create interaction script and test scene

---
+++ Assets/Scripts/InteractionTrigger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class InteractionTrigger : MonoBehaviour
+{
+    public delegate void InteractTriggerHandler();
+    public event InteractTriggerHandler OnInteract;
+    public float interactionDistance = 3;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private KeyCode triggerKey = KeyCode.E;
+
+    private bool awaitingKeyUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(triggerKey))
+        {
+            if (awaitingKeyUp || Vector3.Distance(playerTransform.position, transform.position) > interactionDistance)
+                return;
+            awaitingKeyUp = true;
+            OnInteract?.Invoke();
+        }
+        else
+            awaitingKeyUp = false;
+    }
+}
