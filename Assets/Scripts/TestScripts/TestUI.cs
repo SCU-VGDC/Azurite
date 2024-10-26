671294d51bb45c54f46b317e726d964decf7bfb5
Rand0mNigel <nigelsmeyer@icloud.com>
2024-10-25 18:05:47 -0700

    Moved files in main
    I am so sorry

--- Assets/Scripts/TestScripts/TestUI.cs
+++ Assets/Scripts/TestScripts/TestUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+public class TestUI : MonoBehaviour 
 {
     public GameObject InventoryMenu;
     private bool menuActivated;
