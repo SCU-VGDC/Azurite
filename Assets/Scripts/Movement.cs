22f5a34d5e5886c1c34dedd77c71021e98036224
Viziaxi <56894726+Viziaxi@users.noreply.github.com>
2024-10-28 14:43:24 -0700

    Normalize vectors, fix dt multiplication

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,10 @@ public class Movement : MonoBehaviour
     [SerializeField] Rigidbody2D PlayerRigidBody;
     Vector2 speed = new(0, 0);
     Vector2 input = new(0,0);
-    Vector2 movement = new(0,0);
-    const float minFriction = 0.009f;
-    const float maxFriction = 0.04f;
     //Values have ranges on them to ensure sane values and to ensure NAN or infinity conditions are never encountered
-    [SerializeField] [Range(maxFriction, minFriction)] float friction = 0.1f;
+    [SerializeField] [Range(1, 20)] float deceleration = 0.1f;
     [SerializeField] [Range(1, 50)] float playerMaxSpeed = 50;
-    [SerializeField] [Range(1f, 3)] float multiplyer = 1f;
+    [SerializeField] [Range(1f, 10)] float acceleration = 1f;
     
 
     //Start is called before the first frame update
@@ -31,31 +29,20 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-	//Friction code to have player come to stop when no key pressed
-	//Checks that key is pressed using input vect and checks that movement vect is beyond the minFriction threshold
-      	if(input.x == 0 && (movement.x > minFriction || movement.x < -minFriction)){
-		speed.x += (speed.x*friction)*-1;
-	//At certian point zeroing out player movement is not noticable to the player's eye
-	} else if (input.x == 0){
-		speed.x = 0;
-	}
-	if(input.y == 0 && (movement.y > minFriction || movement.y < -minFriction)){
-		speed.y += (speed.y*friction)*-1;
-	} else if (input.y == 0){
-		speed.y = 0;
-	}
+        input = input.normalized;
 
+        //Friction code to have player come to stop when no key pressed
+        float decelNow = Time.deltaTime * deceleration;
+        if (speed.magnitude > decelNow)
+            speed += decelNow * new Vector2(input.x == 0 ? -MathF.Sign(speed.x) : 0, input.y == 0 ? -MathF.Sign(speed.y) : 0).normalized;
+        else if (input.magnitude == 0)
+            //At certian point zeroing out player movement is not noticable to the player's eye
+            speed = Vector2.zero;
 
-	//applies force to player if they are not moving faster than whatever playerMaxSpeed is set to
-	if(movement.x <= playerMaxSpeed && movement.x >= -playerMaxSpeed){
-		speed.x += input.x * multiplyer; 
-	}
-	if(movement.y <= playerMaxSpeed && movement.y >= -playerMaxSpeed){
-		speed.y += input.y * multiplyer;
-	}
+        //applies force to player if they are not moving faster than whatever playerMaxSpeed is set to
+        speed += Time.deltaTime * acceleration * input;
+        speed = speed.normalized * MathF.Min(speed.magnitude, playerMaxSpeed);
 
-	movement = speed;
-        movement *= Time.deltaTime;
-        PlayerRigidBody.velocity = movement;
+        PlayerRigidBody.velocity = speed;
     }
 }
