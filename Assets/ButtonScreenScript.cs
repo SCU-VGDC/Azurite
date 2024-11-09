935b2f8f265af90dff195e5e6b096bcc3159e016
Rand0mNigel <nigelsmeyer@icloud.com>
2024-11-08 16:58:27 -0800

    Buttons

--- Assets/ButtonScreenScript.cs
+++ Assets/ButtonScreenScript.cs
@@ -10,12 +10,29 @@ public class ButtonScreenScript : MonoBehaviour
     int HeightOffset = 100;
     [SerializeField] GameObject buttonPrefab;
     GameObject[] buttons;
+    [SerializeField] bool toggleNearby;
+    [SerializeField] bool SolutionIsAllOn;
+    public enum ButtonState
+    {
+        OffNoGlass,
+        OnNoGlass,
+        OffWithGlass,
+        OnWithGlass,
+    }
+    
+    public enum SolutionState
+    {
+        Off,
+        On,
+    }
 
-    
-    
+    [SerializeField] ButtonState[] LoadButtons;
+    [SerializeField] SolutionState[] Solution;
+
     // Start is called before the first frame update
     void Start()
     {
+        int buttonCounter = 0;
         buttons= new GameObject[width * height];
         for (int i = 0; i < height; i++)
         {
@@ -26,36 +43,112 @@ public class ButtonScreenScript : MonoBehaviour
                 buttonPos.y -= (75 * i) - HeightOffset;
                 Debug.Log("Set up button number" + (width * i + j));
                 buttons[width * i + j] = Instantiate(buttonPrefab, buttonPos, this.transform.rotation ,this.transform);
-                buttons[width * i + j].GetComponent<ButtonScript>().setUp(width * i + j, this.gameObject);
+                buttons[width * i + j].GetComponent<ButtonScript>().setUp(width * i + j, this.gameObject, LoadButtons[buttonCounter % LoadButtons.Length]);
+                buttonCounter++;
 
             }
         }
-    }
+    }   
 
     public void updateButtons(int bnum)
     {
         buttons[bnum].GetComponent<ButtonScript>().ToggleState();
-        if (bnum + width < width * height)
+        if (toggleNearby)
         {
-            buttons[bnum+ width].GetComponent<ButtonScript>().ToggleState();
-        }
-        if (bnum - width >= 0)
-        {
-            buttons[bnum - width].GetComponent<ButtonScript>().ToggleState();
-        }
-        if ((bnum % width) - 1 >= 0){
-            buttons[bnum - 1].GetComponent<ButtonScript>().ToggleState();
-        }
-        if ((bnum % width) + 1 < width){
-            buttons[bnum + 1].GetComponent<ButtonScript>().ToggleState();
 
 
+            if (bnum + width < width * height)
+            {
+                buttons[bnum+ width].GetComponent<ButtonScript>().ToggleState();
+            }
+            if (bnum - width >= 0)
+            {
+                buttons[bnum - width].GetComponent<ButtonScript>().ToggleState();
+            }
+            if ((bnum % width) - 1 >= 0){
+                buttons[bnum - 1].GetComponent<ButtonScript>().ToggleState();
+            }
+            if ((bnum % width) + 1 < width){
+                buttons[bnum + 1].GetComponent<ButtonScript>().ToggleState();
+
+
+            }
+            
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool isAllOn()
     {
         
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (!buttons[i].GetComponent<ButtonScript>().CheckStatus())
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public bool CheckSolution()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            SolutionState button; //Solution[i % Solution.Length];
+            if (buttons[i].GetComponent<ButtonScript>().CheckStatus())
+            {
+                button = SolutionState.On;
+            }
+            else
+            {
+                button = SolutionState.Off;
+            }
+            if (button != Solution[i % Solution.Length])
+            {
+                return false;
+            }
+
+        }
+
+
+        return true;
+    }
+
+    public void TestSolution()
+    {
+        if (SolutionIsAllOn)
+        {
+            if (isAllOn())
+            {
+                Debug.Log("Correct");
+            }
+            else
+            {
+                Debug.Log("Wrong");
+            }
+            return;
+        }
+        else
+        {
+            if (CheckSolution())
+            {
+                Debug.Log("Correct");
+
+            }
+            Debug.Log("Wrong");
+
+        }
+
+    }
+
+
+    public void OpenScreen()
+    {
+        this.gameObject.SetActive(true);
+    }
+    public void CloseScreen()
+    {
+        this.gameObject.SetActive(false);
     }
 }
--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -5,27 +5,47 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-
+    bool IsGlass;
     bool IsOn;
     int buttonNumber;
     GameObject buttonScreen;
     
     
     // Start is called before the first frame update
-    void Start()
-    {
-        SetOff();
-    }
 
-    public void setUp(int bnum, GameObject screen)
+
+    public void setUp(int bnum, GameObject screen, ButtonScreenScript.ButtonState bs)
     {
         buttonNumber = bnum;
         buttonScreen = screen;
+        switch(bs)
+        {
+            case ButtonScreenScript.ButtonState.OffNoGlass:
+                SetOff();
+                IsGlass= false;
+                break;
+            case ButtonScreenScript.ButtonState.OffWithGlass:
+                SetOff();
+                IsGlass= true;
+                break;
+            case ButtonScreenScript.ButtonState.OnNoGlass:
+                SetOn();
+                IsGlass= false;
+                break;
+            case ButtonScreenScript.ButtonState.OnWithGlass: 
+                SetOn();
+                IsGlass= true;
+                break;
+        }
     }
 
     public void Clicked()
     {
-        buttonScreen.GetComponent<ButtonScreenScript>().updateButtons(buttonNumber);
+        if(!IsGlass)
+        {
+            buttonScreen.GetComponent<ButtonScreenScript>().updateButtons(buttonNumber);
+
+        }
     }
 
 
@@ -54,7 +74,10 @@ public class ButtonScript : MonoBehaviour
         IsOn= false;
 
     }
-
+    public bool CheckStatus()
+    {
+        return IsOn;
+    }
 
 
 
