91760c206c1902c03c60f23ffe1900d226bd0f42
Viziaxi <56894726+Viziaxi@users.noreply.github.com>
2025-01-17 19:05:12 -0800

    update normalization

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -9,7 +9,6 @@ public class Movement : MonoBehaviour
     Vector2 playerInput;
     //Values have ranges on them to ensure sane values and to ensure NAN or infinity conditions are never encountered
     [SerializeField] [Range(0, 10)] float playerSpeed = 1.0f;
-    [SerializeField] [Range(0, 1)] float playerMaxSpeed = 0.7f;
 
     //Update is called once per frame
     void Update()
@@ -20,11 +19,6 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        // prevents player from moving faster diagonally by forcing them to go at 70% the normal speed instead
-        if (playerInput.x != 0 && playerInput.y != 0) {
-            playerInput *= playerMaxSpeed;
-        }
-
-        PlayerRigidBody.velocity = playerInput * playerSpeed; // without this line, player cannot move. at all.
+        PlayerRigidBody.velocity = playerInput.normalized * playerSpeed; // without this line, player cannot move. at all.
     }
 }
