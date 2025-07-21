9132577bb999fb544446a4ccc2eef7d9ebf9eb32
o0llaala <chelsiegnanasekaran@gmail.com>
2025-07-21 00:26:55 -0700

    implemented ambiance
    ambience + ambience game obj + event emitter to main camera (olaceholder)

---
+++ Assets/Scripts/FMOD/AmbiencePlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiencePlayer : MonoBehaviour
+{
+    FMOD.Studio.EventInstance AmbienceInst;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AmbienceInst = FMODUnity.RuntimeManager.CreateInstance("event:/Ambience/Ambience");
+        AmbienceInst.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnDestroy()
+    {
+        AmbienceInst.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+} // end
