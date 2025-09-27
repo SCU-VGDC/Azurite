7ed84baf6ac317ed53b53380bfd41a1d5f836e0b
kieranpazmino <42kpaz@gmail.com>
2025-09-26 18:39:50 -0700

    Add dynamic sorting order to Player sprite
    The greenhouse now has the ability for a player to walk through and go in front of and behind various objects. Z-Level sorting has been discarded in favor of Sorting Order.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,7 +7,8 @@ using UnityEngine.Events;
 public class Player : MonoBehaviour
 {
 	[SerializeField] Rigidbody2D PlayerRigidBody;
-	public List<GameObject> YLevels;
+	[SerializeField] SpriteRenderer spriteRenderer;
+	private List<GameObject> YLevels;
 	Vector2 playerInput;
 	//Values have ranges on them to ensure sane values and to ensure NAN or infinity conditions are never encountered
 	[SerializeField] [Range(0, 10)] float playerSpeed = 1.0f;
@@ -82,5 +83,10 @@ public class Player : MonoBehaviour
 	void FixedUpdate()
 	{
 		PlayerRigidBody.velocity = playerInput.normalized * playerSpeed; // without this line, player cannot move. at all.
+
 	}
+    void LateUpdate()
+    {
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y*100);
+    }
 }
