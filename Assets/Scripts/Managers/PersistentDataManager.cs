b5c9f614258109889382716004f3b75d7671ef0e
Viziaxi <56894726+Viziaxi@users.noreply.github.com>
2025-10-03 16:28:06 -0700

    add events to PersistentDataManager

--- Assets/Scripts/Managers/PersistentDataManager.cs
+++ Assets/Scripts/Managers/PersistentDataManager.cs
@@ -1,12 +1,13 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PersistentDataManager : MonoBehaviour
 {
     public static PersistentDataManager Instance;
-
-    [SerializeField] private Dictionary<string, object> persistentDict = new Dictionary<string, object>();
+    
+    [SerializeField] private Dictionary<string, object> persistentDict = new();
+    private readonly Dictionary<string, Delegate> eventStore = new();
 
     // basic singleton pattern
     private void Awake()
@@ -27,9 +28,25 @@ public class PersistentDataManager : MonoBehaviour
         Instance.Set("worldState", 0);
     }
 
-    public void Set(string key, object value)
+    public void Set<T>(string key, T value)
     {
+        T oldValue = default;
+        if (persistentDict.TryGetValue(key, out var oldValueObj))
+        {
+            if (oldValueObj is not T)
+            {
+                Debug.LogError($"'{key}' is not a {typeof(T)}!");
+                return;
+            }
+            oldValue = (T)oldValueObj;
+        }
+
         persistentDict[key] = value;
+
+        if (eventStore.TryGetValue(key, out var func))
+        {
+            func.DynamicInvoke(value, oldValue);
+        }
     }
 
     public bool Remove(string key)
@@ -54,12 +71,32 @@ public class PersistentDataManager : MonoBehaviour
 
     public bool TryGet<T>(string key, out T value)
     {
-        if (persistentDict.TryGetValue(key, out object obj) && obj is T)
+        if (persistentDict.TryGetValue(key, out object obj) && obj is T castObj)
         {
-            value = (T)obj;
+            value = castObj;
             return true;
         }
         value = default;
         return false;
+    }
+
+    public void ListenForKeyChanged<T>(string key, Action<T,T> action)
+    {
+        if (eventStore.TryGetValue(key, out var func))
+            eventStore[key] = Delegate.Combine(func, action);
+        else
+            eventStore[key] = action;
+    }
+
+    public void StopListeningForKeyChanged(string key, Delegate action)
+    {
+        if (eventStore.TryGetValue(key, out var func))
+        {
+            var newDelegate = Delegate.Remove(func, action);
+            if (newDelegate == null)
+                eventStore.Remove(key);
+            else
+                eventStore[key] = newDelegate;
+        }
     }
 }
