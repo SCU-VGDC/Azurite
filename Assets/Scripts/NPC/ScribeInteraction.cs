f0a8378b0c2a3da4c673d430c0a2108e2cf1d938
Viziaxi <56894726+Viziaxi@users.noreply.github.com>
2025-11-14 19:06:49 -0800

    scribe interaction/dialogue

--- Assets/Scripts/NPC/ScribeInteraction.cs
+++ Assets/Scripts/NPC/ScribeInteraction.cs
@@ -3,20 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(DialogueSequence))]
 [RequireComponent(typeof(InteractionTrigger))]
 public class ScribeInteraction : MonoBehaviour
 {
-    private DialogueSequence dialogue;
-    [SerializeField] private ScribeQuizPaper quizPaper;
+    [SerializeField] private DialogueSequence firstMeetDialogue, incorrectDialogue, correctDialogue, repeatAfterCorrectDialogue;
+    [SerializeField] private ScribeQuizPaper quizPaperUI;
+    [SerializeField] private Item quizPaperItem;
 
-    void Awake()
-    {
-        dialogue = GetComponent<DialogueSequence>();
-    }
+    private bool quizSolved = false;
 
     public void OnInteract()
     {
-        dialogue.DialogueStart();
+        if (GameManager.inst.player.Inventory.HasItem(quizPaperItem))
+        {
+            if (quizPaperUI.IsCorrect)
+                correctDialogue.DialogueStart();
+            else
+                correctDialogue.DialogueStart();
+        }
+        else
+        {
+            if (!quizSolved)
+                firstMeetDialogue.DialogueStart();
+            else
+                repeatAfterCorrectDialogue.DialogueStart();
+        }
+    }
+
+    public void OnQuizSolved()
+    {
+        quizSolved = true;
+    }
+
+    public void GiveQuizItem()
+    {
+        GameManager.inst.player.Inventory.AddItem(quizPaperItem, 1);
     }
 }
