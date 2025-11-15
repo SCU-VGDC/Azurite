cd158c3404f376560df2f4a6378b37db436f1e41
kieranpazmino <42kpaz@gmail.com>
2025-11-14 18:05:34 -0800

    Add InteractCanvas prefab and update UI/Flower system
    Added new InteractCanvas prefab for interaction prompts and integrated it into GreenHouseRoomScene. Updated Canvas prefab settings and improved text rendering options. Refactored Dialogue prefabs into Button/Dialogue directory. Enhanced FlowerSystem prefab with new collider and renderer properties. Added FlowerMenuController script for UI control.

--- Assets/Scripts/Environment/InteractionTrigger.cs
+++ Assets/Scripts/Environment/InteractionTrigger.cs
@@ -4,6 +4,9 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [RequireComponent(typeof(Collider2D))]
 public class InteractionTrigger : MonoBehaviour, IComparable<InteractionTrigger>
@@ -12,6 +15,9 @@ public class InteractionTrigger : MonoBehaviour, IComparable<InteractionTrigger>
     public KeyCode triggerKey = KeyCode.E;
     public TextMeshProUGUI textPopUp;
     public int ActionCount = 0;
+    
+    private static readonly string INTERACT_CANVAS_PREFAB_PATH = "Assets/Prefabs/UI/InteractCanvas.prefab";
+    private static readonly string INTERACT_CANVAS_RESOURCES_PATH = "Prefabs/UI/InteractCanvas";
 
     public int CompareTo(InteractionTrigger other)
     {
@@ -21,14 +27,126 @@ public class InteractionTrigger : MonoBehaviour, IComparable<InteractionTrigger>
         return MathF.Sign(myDist - otherDist);
     }
 
+    void Reset()
+    {
+        // Called when component is first added to a GameObject in the editor
+        SetupInteractCanvas();
+    }
+
+    void Awake()
+    {
+        // Runtime fallback: if textPopUp is not assigned, instantiate the InteractCanvas prefab
+        if (textPopUp == null)
+        {
+            SetupInteractCanvas();
+        }
+    }
+
+    private void SetupInteractCanvas()
+    {
+        // Don't create if already exists
+        if (textPopUp != null)
+        {
+            return;
+        }
+
+        // Check if canvas already exists as a child
+        Transform existingCanvas = transform.Find("InteractCanvas");
+        if (existingCanvas != null)
+        {
+            Canvas existingCanvasComponent = existingCanvas.GetComponent<Canvas>();
+            if (existingCanvasComponent != null)
+            {
+                existingCanvasComponent.sortingOrder = 32767; // Ensure high sorting order
+            }
+            textPopUp = existingCanvas.GetComponentInChildren<TextMeshProUGUI>(true);
+            if (textPopUp != null)
+            {
+                return;
+            }
+        }
+
+        GameObject prefabToInstantiate = null;
+
+#if UNITY_EDITOR
+        // In editor (both playing and not playing), try asset path first
+        if (!Application.isPlaying)
+        {
+            prefabToInstantiate = AssetDatabase.LoadAssetAtPath<GameObject>(INTERACT_CANVAS_PREFAB_PATH);
+        }
+        else
+        {
+            // Editor play mode: try Resources first, then asset path as fallback
+            prefabToInstantiate = Resources.Load<GameObject>(INTERACT_CANVAS_RESOURCES_PATH);
+            if (prefabToInstantiate == null)
+            {
+                prefabToInstantiate = AssetDatabase.LoadAssetAtPath<GameObject>(INTERACT_CANVAS_PREFAB_PATH);
+            }
+        }
+#else
+        // In build, load from Resources
+        prefabToInstantiate = Resources.Load<GameObject>(INTERACT_CANVAS_RESOURCES_PATH);
+#endif
+
+        if (prefabToInstantiate != null)
+        {
+            GameObject canvasInstance;
+            
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                // In edit mode (not playing), use PrefabUtility to instantiate
+                canvasInstance = (GameObject)PrefabUtility.InstantiatePrefab(prefabToInstantiate, transform);
+                Undo.RegisterCreatedObjectUndo(canvasInstance, "Create InteractCanvas");
+            }
+            else
+#endif
+            {
+                // At runtime (editor play mode or build), use regular Instantiate
+                canvasInstance = Instantiate(prefabToInstantiate, transform);
+            }
+            
+            canvasInstance.name = "InteractCanvas";
+            
+            // Set high sorting order on the Canvas to render on top
+            Canvas canvas = canvasInstance.GetComponent<Canvas>();
+            if (canvas != null)
+            {
+                canvas.sortingOrder = 32767; // Maximum sorting order to ensure it renders on top
+            }
+            
+            // Find the TextMeshProUGUI component in the instantiated prefab
+            textPopUp = canvasInstance.GetComponentInChildren<TextMeshProUGUI>(true);
+            
+            if (textPopUp == null)
+            {
+                Debug.LogError($"InteractionTrigger: Could not find TextMeshProUGUI component in instantiated InteractCanvas prefab on {gameObject.name}");
+            }
+#if UNITY_EDITOR
+            else if (!Application.isPlaying)
+            {
+                // Mark the object as dirty so changes are saved
+                EditorUtility.SetDirty(this);
+            }
+#endif
+        }
+        else
+        {
+            Debug.LogError($"InteractionTrigger: Could not load InteractCanvas prefab at {INTERACT_CANVAS_PREFAB_PATH} on {gameObject.name}");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Canvas canvas = textPopUp.GetComponentInParent<Canvas>(true);
-        canvas.transform.localPosition = new Vector3(0, 1, 0);
-        canvas.worldCamera = Camera.main;
-        textPopUp.text = triggerKey.ToString();
-        ToggleTextPopup(false);
+        if (textPopUp != null)
+        {
+            Canvas canvas = textPopUp.GetComponentInParent<Canvas>(true);
+            canvas.transform.localPosition = new Vector3(0, 1, 0);
+            canvas.worldCamera = Camera.main;
+            textPopUp.text = triggerKey.ToString();
+            ToggleTextPopup(false);
+        }
     }
 
     public void Trigger()
@@ -42,6 +160,9 @@ public class InteractionTrigger : MonoBehaviour, IComparable<InteractionTrigger>
 
     public void ToggleTextPopup(bool value)
     {
-        textPopUp.GetComponentInParent<Canvas>(true).gameObject.SetActive(value);
+        if (textPopUp != null)
+        {
+            textPopUp.GetComponentInParent<Canvas>(true).gameObject.SetActive(value);
+        }
     }
 }
