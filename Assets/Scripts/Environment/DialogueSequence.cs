38241fe143919771a7d91e5c86f5a4dcf079e3f9
Viziaxi <56894726+Viziaxi@users.noreply.github.com>
2025-11-14 18:52:09 -0800

    new quiz journal UI

--- Assets/Scripts/Environment/DialogueSequence.cs
+++ Assets/Scripts/Environment/DialogueSequence.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using Dialogue.Data;
+
 namespace Dialogue.Data 
 {
     [Serializable]
--- Assets/Scripts/UI/ScribeQuiz/ScribeQuizPaper.cs
+++ Assets/Scripts/UI/ScribeQuiz/ScribeQuizPaper.cs
@@ -2,83 +2,106 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 [RequireComponent(typeof(CanvasGroup))]
+[RequireComponent(typeof(Canvas))]
 public class ScribeQuizPaper : MonoBehaviour
 {
+    [Serializable]
     public class QuizQuestion
     {
         public string question;
         public string[] answers;
         public int correctIndex;
+        [NonSerialized] public int selectedIndex = -1;
     }
 
     [SerializeField] private GameObject answerPrefab;
     [SerializeField] private TextMeshProUGUI questionText;
     [SerializeField] private VerticalLayoutGroup verticalLayout;
-    [SerializeField] private ScribeInteraction scribe;
     [SerializeField] private QuizQuestion[] questions;
+    [SerializeField] private Button prevButton, nextButton;
 
     private int currentQuestion;
 
-    private void SetupQuestionDisplay()
+    public bool IsCorrect
     {
+        get
+        {
+            foreach (var question in questions)
+            {
+                if (question.selectedIndex != question.correctIndex)
+                    return false;
+            }
+            return true;
+
+        }
+    }
+
+    private void Start()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+        prevButton.onClick.AddListener(() => TrySetQuestionIndex(currentQuestion - 1));
+        nextButton.onClick.AddListener(() => TrySetQuestionIndex(currentQuestion + 1));
+        TrySetQuestionIndex(0);
+    }
+
+    private bool TrySetQuestionIndex(int newIndex)
+    {
+        newIndex = Math.Clamp(newIndex, 0, questions.Length - 1);
+        if (newIndex == currentQuestion)
+            return false;
+
+        currentQuestion = newIndex;
+        prevButton.interactable = currentQuestion > 0;
+        nextButton.interactable = currentQuestion < questions.Length - 1;
+        DisplayCurrentQuestion();
+        return true;
+    }
+
+    private void DisplayCurrentQuestion()
+    {
+        questionText.text = questions[currentQuestion].question;
+
         foreach (Transform child in verticalLayout.transform)
-            if (child.gameObject != questionText.gameObject)
-                Destroy(child.gameObject);
+            Destroy(child.gameObject);
 
         var questionInfo = questions[currentQuestion];
         for (int i = 0; i < questionInfo.answers.Length; i++)
         {
+            int qNumCopy = currentQuestion;
+            int aNumCopy = i;
             TextMeshProUGUI text = Instantiate(answerPrefab, verticalLayout.transform).GetComponent<TextMeshProUGUI>();
             text.text = (char)(65 + i) + ") " + questionInfo.answers[i];
-            text.GetComponent<Button>().onClick.AddListener(() => OnAnswer(i));
+            text.GetComponent<Button>().onClick.AddListener(() => OnAnswer(qNumCopy, aNumCopy));
         }
     }
 
-    private void OnAnswer(int answerNum)
+    private void OnAnswer(int questionNum, int answerNum)
     {
-        foreach (Transform child in verticalLayout.transform)
-            if (child.TryGetComponent(out Button button))
-                button.onClick.RemoveAllListeners();
-        if (answerNum == questions[currentQuestion].correctIndex)
-        {
-            OnCorrect();
-        }
-        else
-        {
-            OnIncorrect();
-        }
-    }
-
-    private void OnCorrect()
-    {
-        currentQuestion++;
-        if (currentQuestion == questions.Length)
-        {
-
-        }
-        else
-        {
-            
-        }
-    }
-
-    private void OnIncorrect()
-    {
-        currentQuestion = 0;
+        questions[questionNum].selectedIndex = answerNum;
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
-        GetComponent<CanvasGroup>().DOFade(1f, 0.3f);
+        var group = GetComponent<CanvasGroup>();
+        group.DOFade(1f, 0.3f);
+        group.interactable = true;
+        group.blocksRaycasts = true;
     }
 
     public void Hide()
     {
+        var group = GetComponent<CanvasGroup>();
         DOTween.Sequence()
-            .Append(GetComponent<CanvasGroup>().DOFade(0f, 0.3f))
-            .AppendCallback(() => gameObject.SetActive(false));
+            .Append(group.DOFade(0f, 0.3f))
+            .AppendCallback(() =>
+            {
+                group.interactable = false;
+                group.blocksRaycasts = false;
+                gameObject.SetActive(false);
+            });
     }
 }
